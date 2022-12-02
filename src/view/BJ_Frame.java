ab32266603f88b2bdeff51bbe012319f1a350fde
David Herzog <fernando.10.10@icloud.com>
2022-12-02 20:23:55 +0100

    V1.0 // gifs added / scoreLabelText modified

--- src/view/BJ_Frame.java
+++ src/view/BJ_Frame.java
@@ -4,7 +4,10 @@ import java.awt.Color;
 import java.awt.Font;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.net.URL;
 
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.SwingConstants;
@@ -21,39 +24,61 @@ public class BJ_Frame extends JFrame implements	KeyListener{
 
 	private static final long serialVersionUID = 1L;
 	
-	JLabel label = new JLabel("The Label", SwingConstants.CENTER);
+	JLabel label = new JLabel();
 	private int score;
 
 	public BJ_Frame(){
 		score = 0;
 		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
 		this.setTitle("21 Slim");
-		this.setSize(300,150);
-		label.setBackground(Color.DARK_GRAY);
-		label.setOpaque(true);
-		label.setText("Score: " + score);
-		label.setForeground(Color.white);
+		this.setSize(316,189);
+		setImageAndFontColor();
+//		label.setIcon(new ImageIcon("src/view/images/lame.gif"));
+//		label.setBackground(Color.DARK_GRAY);
+//		label.setOpaque(true);
+		label.setText(getScoreToString());
+//		label.setForeground(Color.white);
 		label.setFont(new Font("Playbill", Font.PLAIN, 80));
+		label.setHorizontalTextPosition(JLabel.CENTER);
+		label.setVerticalTextPosition(JLabel.CENTER);
 		this.add(label);
 		this.addKeyListener(this);
 		this.setVisible(true);
 	}
+	
+	public void setImageAndFontColor() {
+		if (score < -4) {
+			label.setIcon(new ImageIcon("src/view/images/ice.gif"));
+			label.setForeground(new Color(139,69,19));
+		}else if (score < 5) {
+			label.setIcon(new ImageIcon("src/view/images/lame.gif"));
+			label.setForeground(Color.black);
+		}else {
+			label.setIcon(new ImageIcon("src/view/images/hot.gif"));
+			label.setForeground(Color.white);
+		}
+	}
 
 	public void highCard() {
 		score --;
-	
-		label.setText("Score: " + score);
+		setImageAndFontColor();
+		label.setText(getScoreToString());
 	}
-
 	
 	public void lowCard() {
 		score++;
-		label.setText("Score: " + score);
+		setImageAndFontColor();
+		label.setText(getScoreToString());
+	}
+	
+	public String getScoreToString() {
+		return score > 0 ? "Score: +" + score : "Score: " + score; 
 	}
 	
 	@Override
 	public void keyPressed(KeyEvent e) {
 		switch (e.getKeyCode()) {
+		
 		case 37 : lowCard();  break;
 		case 38 : highCard(); break;
 		case 39 : highCard(); break;
